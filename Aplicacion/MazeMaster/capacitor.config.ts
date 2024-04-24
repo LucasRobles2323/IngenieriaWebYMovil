43324972ff6600b4a57261c2959b3dd7f8e98ca7
Lucas <lucasrobles2323@gmail.com>
2024-04-24 13:39:56 -0400

    Ionic

---
+++ Aplicacion/MazeMaster/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MazeMaster',
+  webDir: 'www'
+};
+
+export default config;
