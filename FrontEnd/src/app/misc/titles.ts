e3c79b5b233c64e57dde9dae11ba338dfd63cc9d
Lucas <lucasrobles2323@gmail.com>
2024-06-24 16:43:52 -0400

    Updates
    Se creo un script para instalar las librerias de python que se usan en el back-end.
    Se elimino un ts de misc que ya no tenia uso

--- FrontEnd/src/app/misc/titles.ts
+++
@@ -1,8 +0,0 @@
-export const routesTitles: { [key: string]: string } = {
-    '/control-robot': 'Control Remoto',
-    '/inicio-sesion': 'Inicio de Sesión',
-    '/inicio-sesion/registro-usuario': 'Registro de Usuario',
-    '/inicio-sesion/sesion-usuario': 'Sesion Iniciada',
-    '/resolver-laberinto': 'Laberinto',
-    '/resolver-laberinto/laberinto-resuelto': 'Ruta Encontrada',
-};
